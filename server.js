f38271949a1d4c8d033653b029f3106016fa7f7a
maheryhaja <ygunion@gmail.com>
2017-10-18 00:01:00 +0300

    actualisation des logs 2

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/webhook', function(req, res) {
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
-  log = "nouvelle requete ftsn";
+  log = "nouvelle requete ftsn "+data.object;
 
   // Make sure this is a page subscription
   if (data.object === 'page') {
